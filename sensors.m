aa69981954fd5f330035229e8083cacbf7a77c51
VictorSchonk <victorschonk@hotmail.com>
2018-05-01 09:09:17 +0200

    .

--- sensors.m
+++ sensors.m
@@ -4,9 +4,13 @@ function [outputArg1,outputArg2] = sensors()
 
 	str = EPOCommunications('transmit','Sd');
 	st = strsplit(str,'U');
-	s = st(4:end);
-	outputArg1 = s(1);
-	outputArg2 = s(2);
+	st = st(2:3);
+	s1 = strsplit(string(st(1)),'L');
+	s2 = strsplit(string(st(2)),'R');
+	s1 = strsplit(s1(2),'\n');
+	s2 = strsplit(s2(2),'\n');
+	outputArg1 = str2double(s1(1));
+	outputArg2 = str2double(s2(1));
 	
 end
 
