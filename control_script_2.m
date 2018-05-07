f6ae9ab97fed35a217a52d907efbcbdabe7988a4
VictorSchonk <victorschonk@hotmail.com>
2018-05-07 22:19:03 +0200

    ..

--- control_script_2.m
+++ control_script_2.m
@@ -2,26 +2,26 @@ p1 = 1;
 p2 = 1;
 
 v = 0; % begin speed
-vmin = 0.15; % speed to stop decelerating at
 a165 = 1.25; % acceleration with motor at full power
 a135 = -6.5; % acceleration with motor full power backwards while moving forwards
-dist = 0; %
-del = 0.15; % delay margin
+
 sdist = 0.40; % distance to stop at in meters
+vmin = 0.15; % speed to stop decelerating at
+del = 0.15; % delay margin
 derr = 0.2; % error margin, to avoid overshoot and oscilation
+
 dm = 3; % 3 for 165 | 2 for 150 | 1 for 135
-a = [a135 0 a165];
+a = [a135 0 a165]; % acceleration for different motor states
 
 t0 = tic;
 drive(165);
 while p1
 	
-	v = toc*a(dm);
-	dist = 0.5*v*toc;
+	v = toc*a(dm); % current speed of the vehicle
 	dmar = del*v; % distance to compensate the delay
 	
 	if min(sensors()/100) < sdist + dmar % currently the minimal sensor value is used mean is another option
-		p1 = 0;
+		break;
 	end
 	
 end
@@ -41,7 +41,7 @@ while p2
 	elseif mean(sensors()/100) < sdist+derr
 		drive(150);
 		dm = 2;
-		p2 = 0;
+		break;
 	else
 		drive(165);
 		dm = 3;
