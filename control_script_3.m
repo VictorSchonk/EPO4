6952a4a0e2cb3ddd29d4460838cb7099c1da5c6a
VictorSchonk <victorschonk@hotmail.com>
2018-05-15 12:25:42 +0200

    FINAL!!!

--- control_script_3.m
+++ control_script_3.m
@@ -2,21 +2,21 @@ cport = 6; % com port to use
 
 dbumper = 0.08; % distance between sensors and bumper
 
-sdist = 0.30; % distance to stop at in meters
-vroll = 0.55; % speed to aproach the final position [m/s] 0.46 @ 154 | 0.56 @ 155
+sdist = 0.50; % distance to stop at in meters
+vroll = 0.4; % speed to aproach the final position [m/s] 0.46 @ 154 | 0.56 @ 155
 vmin = 0.15; % speed to stop decelerating at
 del = 0.3;%0.2; % delay margin
 derr = -0.00;%0.6-sdist; % error margin, to avoid overshoot and oscilation
 
 v = 0; % begin speed
-a165 = 1.23; % acceleration with motor at full power
-a135 = -6.45; % acceleration with motor full power backwards while moving forwards
+a165 = 1.217; % acceleration with motor at full power
+a135 = -6.57; % acceleration with motor full power backwards while moving forwards
 % assumed that with motor at 154 with low speeds there is no acceleration,
 % nor decelleration
 
 %%% Compensation for unexplained overshoot, lineairly proportional to final
 %%% distance
-tmp = linspace(0.1,0.3,40); % add 0.05 for short begin dist and substr 0.05 for long begin dist
+tmp = linspace(0.23,0.23,40); % add 0.05 for short begin dist and substr 0.05 for long begin dist
 dos = tmp(int8((sdist-0.29)*100)); %+dbumper
 clear tmp;
 
@@ -36,7 +36,7 @@ while 1
 	dmar = del*v; % distance to compensate the delay
 	r = r+1;
 	if r > r_val
-		if min(sensors()/100) < 1.3+dmar % currently the minimal sensor value is used mean is another option
+		if mean(sensors()/100) < 1.5+dmar % currently the minimal sensor value is used mean is another option
 			break;
 		end
 		r = 0;
@@ -45,13 +45,14 @@ end
 drive(135);
 pause(abs((v-vroll)/a(1)));
 drive(155);
+% pause(0.04);
 v = vroll;
 dmar = del*v;
 while 1
 	r = r+1;
 	if r>r_val
 		if mean(sensors()/100) < sdist + derr + dmar + dos + dbumper
-			drive(144);
+			drive(138);
 			break;
 		end
 		r = 0;
