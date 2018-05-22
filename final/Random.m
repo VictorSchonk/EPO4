8348442506ef7f4c89815db23bd5b0499d0a62f5
Antony-TUDelft <38687495+Antony-TUDelft@users.noreply.github.com>
2018-05-22 11:35:49 +0200

    byhjb

--- final/Random.m
+++ final/Random.m
@@ -1,15 +1,19 @@
+clear all;
 Nbits = 32;
-
+r = 1;
 hold on;
-for i = 1:100
+for i = 1:2000
     seq(i,:) = randi([0 1],1,Nbits);
     auto(i,:) = conv(seq(i,:),fliplr(seq(i,:)));
-    try
-        if findpeaks(auto(i,:),'MinPeakHeight',25)
+        if max(auto(i,:)) >= 25
             y = i
+            f(r,:) = fftshift(abs(fft(auto(i,:))));
+            r=r+1;
+        else
+            auto(i,:) = 0;
         end
-    catch
-        auto(i) = 0;
-    end
     plot(auto(i,:));
 end
+
+figure;
+plot(f);
