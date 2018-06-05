df1eed1827cd927471d038fd83b9b33e48a442dd
VictorSchonk <victorschonk@hotmail.com>
2018-06-05 19:52:37 +0200

    fegragf

---
+++ final/rec_cut.m
@@ -0,0 +1,27 @@
+function [outArg] = rec_cut(inArg)
+%REC_CUT Function to cut an input of size nmic (5 in our case)
+%
+%to thecut_length (11000 in our case)
+%
+%with 50 leading samples
+%	from the point at which it gets higher than thresh (0.01 in our case).
+
+	nmic = 5;			% The amount of recorded channels
+	thresh = 0.01;		% Treshold value to detect when something is transmitted
+	cut_length = 11000;	% length of the cutted up recording
+	
+	try
+		for i = 1:nmic
+			ind(i) = find(inArg(:,i) >= thresh,1);
+			if ind(i) <= 50
+				ind(i) = find(inArg(ind(i)+4000:end,i) >= thresh,1);
+			end
+		end
+		ind(nmic+1) = min(ind);
+		outArg = inArg(ind(nmic+1)-50:ind(nmic+1)+cut_length-50,:);
+	catch
+		outArg = inArg;
+	end
+
+end
+
--- final/record.m
+++ final/record.m
@@ -1,39 +1,15 @@
-function [ rec ] = record()
+function [ rec_out ] = record()
 %RECORD This function is used to simplify the recording of the 0.25 second
 %audio clips and then resize them so that the correct interval is returned.
 %   
 %	rec			| The [4050 x nmic] array of audio data
 	
-	%	setup of the threshold and recording values
-	nmic = 5;			% Amount of microphones being used
-	thresh = 0.01;		% Treshold value to detect when something is transmitted
-	lastchannel = nmic; % microphones to use, as they are numbered
-	cut_length = 11000;		% length of the cutted up recording
+% % 	nmic = 5;			% Amount of microphones being used
 % % 	Fs = 48000;
-    
-% 	setup_beacon(10000,'0x62ffdfff',2500,2500);
-% 	pause(0.1)
-% 	EPOCommunications('transmit','A1');
-% 	pause(0.1)
 	
-	rec = pa_wavrecord(1, lastchannel, 24000,48e3,0,'asio'); % recorded sample for threshold detection
+	rec = pa_wavrecord(1, 5, 24000,48e3,0,'asio'); % recorded sample for threshold detection
 	
-	try
-% 		ind = zeros(nmic+1,1);
-		for i = 1:nmic
-			ind(i) = find(rec(:,i) >= thresh,1);
-			if ind(i) <= 50
-				ind(i) = find(rec(ind(i)+4000:end,i) >= thresh,1);
-			end
-		end
-		ind(nmic+1) = min(ind);
-		rec = rec(ind(nmic+1)-50:ind(nmic+1)+cut_length-50,:);
-	catch
-		rec = rec;
-	end
+	rec_out = rec_cut(rec);
 	
-% 	plot(rec(ind-50:ind+4000));	
-% 	EPOCommunications('transmit','A0');
-% 	closeCom()
 end
 	
