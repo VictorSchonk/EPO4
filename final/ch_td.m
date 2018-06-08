2e29e4224549be170fdfd72fcc915cc8268700c0
VictorSchonk <victorschonk@hotmail.com>
2018-06-08 10:04:20 +0200

    frearrfre

--- final/ch_td.m
+++ final/ch_td.m
@@ -17,7 +17,7 @@ function [outArray] = ch_td(ipArg)
 	Fs = 48000;
 	
 % 	load('data/reference_05-06-2018_2.mat','x'); % Load the refence signal for channel estimation.
-	load('\\tudelft.net\student-homes\k\zkleijweg\My Documents\GitHub\EPO4\final\data\08-06-2018_mic_1.mat','rec');
+	load('data\08-06-2018_mic_1.mat','rec');
 	x = rec(:,1);
 	h1 = abs(ch3(x,ipArg(:,1)));
 	h2 = abs(ch3(x,ipArg(:,2)));
