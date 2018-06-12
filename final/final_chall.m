210ce99776344ca4396cfe5421037420f4c4db99
VictorSchonk <victorschonk@hotmail.com>
2018-06-12 10:23:20 +0200

    fdsa

--- final/final_chall.m
+++ final/final_chall.m
@@ -1,6 +1,7 @@
 cport = 3;		% Com port to use.
 q = 1;
 
+% log = [x,y,dir] % Variable to keep track of x y position
 
 A = [];
 dir = ;
@@ -8,19 +9,24 @@ B = [];
 
 try
 	
+	% SETUP
 	openCom(cport);
-	
 	setup_beacon(10000,'0x62ffdfff',2500,2500);
 	pause(0.1)
 	EPOCommunications('transmit','A1');
 	
+	% FIRST LOCALISATION
 	rec = record();		% Record 24000 samples.
 	times = ch_td(rec);	% Calculate all the times.
 	pos = loc(times);	% Calculate the position.
+	log(1) = [pos(1),pos(2),dir];
 	
+	% FIRST TUrn
 	angB = atan(B(2)/B(1));
 	if abs(angB-dir) <= 90
-		turn(calc_th(pos(1),pos(2),dir,B(1),B(2)));
+		th = calc_th(pos(1),pos(2),dir,B(1),B(2));
+		turn(th);
+		dir = mod((dir+th),360);
 	else
 		turn(180);
 		dir = mod((dir + 180),360);
