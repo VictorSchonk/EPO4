a00c71efd58995b949d1a15cb9f09375257a0c25
VictorSchonk <victorschonk@hotmail.com>
2018-06-12 09:10:01 +0200

    vctjyfh

--- final/final_chall.m
+++ final/final_chall.m
@@ -1,6 +1,11 @@
 cport = 3;		% Com port to use.
 q = 1;
 
+
+A = [];
+dir = ;
+B = [];
+
 try
 	
 	openCom(cport);
@@ -9,11 +14,31 @@ try
 	pause(0.1)
 	EPOCommunications('transmit','A1');
 	
+	rec = record();		% Record 24000 samples.
+	times = ch_td(rec);	% Calculate all the times.
+	pos = loc(times);	% Calculate the position.
+	
+	angB = atan(B(2)/B(1));
+	if abs(angB-dir) <= 90
+		turn(calc_th(pos(1),pos(2),dir,B(1),B(2)));
+	else
+		turn(180);
+		dir = mod((dir + 180),360);
+		
+		rec = record();		% Record 24000 samples.
+		times = ch_td(rec);	% Calculate all the times.
+		pos = loc(times);	% Calculate the position.
+		
+		turn(calc_th(pos(1),pos(2),dir,B(1),B(2)));
+	end
+	
 	while not(q)
 		
 		rec = record();		% Record 24000 samples.
 		times = ch_td(rec);	% Calculate all the times.
 		pos = loc(times);	% Calculate the position.
+		
+		% Drivey drivey stuff
 		
 		break;
 		
@@ -27,6 +52,8 @@ try
 		rec = record();		% Record 24000 samples.
 		times = ch_td(rec);	% Calculate all the times.
 		pos = loc(times);	% Calculate the position.
+		
+		% drivey drivey stuff 2
 		
 		break;
 		
