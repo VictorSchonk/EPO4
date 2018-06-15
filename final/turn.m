5ea7070a8932c8074b97b794841e11ee8aace89c
Zep011 <zepjannes@gmail.com>
2018-06-15 10:16:51 +0200

    turn function and test script

--- final/turn.m
+++ final/turn.m
@@ -1,52 +1,47 @@
 function [ th ] = turn( turndeg, dir, xbegin, ybegin )
 %TURN car makes a turn of inarg degrees
-%     inarg < 0 : left turn
-%     inarg > 0 : right turn
-% 
-% openCom(3)   %
-% pause(0.2)
-% steer(200)
-% pause(0.2)
-% drive(157)  %check
-% pause(0.5)  %vary
-% drive(150)
-% steer(150)
-% stop()
-x = xbegin;
-y = ybegin;
+%     turndeg > 0 : left turn
+%     turndeg < 0 : right turn
+
+rot = [cos(-dir),-sin(-dir);sin(-dir),cos(-dir)];
+posbegin = rot*[xbegin;ybegin];
 r = 76;		%turn radius[cm]
 
-if turndeg < 0		%left turn
+if turndeg > 0		%left turn
 	steer(200)
 	pause(0.2)
 	drive(157)
 	pause(0.5)
 	drive(150)
 	[x,y,~] = position(x,y,0);	%redetermine position
-	th = 90 - (pi/180)*acos((y-ybegin)/r);	%calculate total turned degrees
-	while th > turndeg		%while not turned enough continue
+	pos = rot*[x;y];
+	th = 90 - (pi/180)*acos((pos(y)-posbegin(2))/r);	%calculate total turned degrees
+	while th < turndeg		%while not turned enough continue
 		drive(157)
 		pause(0.5)
 		drive(150)
 		[x,y,~] = position(x,y,0);
-		th = 90 - (pi/180)*acos((y-ybegin)/r);
+		pos = rot*[x;y];
+		th = 90 - (pi/180)*acos((pos(2)-posbegin(2))/r);
 	end
 	steer(150)	%steer straight after turn
 	
-elseif turndeg > 0	%right turn
+elseif turndeg < 0	%right turn
 	steer(100)
 	pause(0.2)
 	drive(157)
 	pause(0.5)
 	drive(150)
 	[x,y,~] = position(x,y,0);	%redetermine position
-	th = 90 - (pi/180)*acos((y-ybegin)/r);	%calculate total turned degrees
-	while th < turndeg		%while not turned enough continue
+	pos = rot*[x;y];
+	th = 90 - (pi/180)*acos((pos(2)-posbegin(2))/r);	%calculate total turned degrees
+	while th > turndeg		%while not turned enough continue
 		drive(157)
 		pause(0.5)
 		drive(150)
 		[x,y,~] = position(x,y,0);
-		th = 90 - (pi/180)*acos((y-ybegin)/r);
+		pos = rot*[x;y];
+		th = 90 - (pi/180)*acos((pos(2)-posbegin(2))/r);
 	end
 	steer(150)
 else				%apparently straight
