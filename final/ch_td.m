f431739e3ac230a4416772cee1b42bd8f8f8beea
Zep011 <zepjannes@gmail.com>
2018-06-20 12:52:00 +0200

    khbasedvf

--- final/ch_td.m
+++ final/ch_td.m
@@ -18,6 +18,7 @@ function [outArray] = ch_td(ipArg,x_co,y_co) %,
 	mf = 3;
 	Fs = 48000;
 	
+	%Distance from the last location to each microphone
 	d1 = sqrt((460-x_co)^2 + (y_co)^2);
 	d2 = sqrt(x_co^2 + y_co^2);
 	d3 = sqrt((x_co)^2 + (460-y_co)^2);
@@ -27,6 +28,15 @@ function [outArray] = ch_td(ipArg,x_co,y_co) %,
 	for i = 1:5
 		if d(i) == min(d)
 			n = i;
+		end
+	end
+	
+	%Determine bin mid to look for channel peak
+	for i = 1:5
+		if i = n
+			b(i) = 90;
+		else
+			b(i) = 90 + d(i)*Fs/34300;
 		end
 	end
 	
@@ -47,25 +57,25 @@ function [outArray] = ch_td(ipArg,x_co,y_co) %,
 	plot(h);
 toc
 	%12
-	r12 = ch_td_pair(h(:,2),h(:,1),mf,Fs);
+	r12 = ch_td_pair(h(:,2),h(:,1),mf,Fs,b(2),b(1));
 	%13
-	r13 = ch_td_pair(h(:,3),h(:,1),mf,Fs);
+	r13 = ch_td_pair(h(:,3),h(:,1),mf,Fs,b(3),b(1));
 	%14
-	r14 = ch_td_pair(h(:,4),h(:,1),mf,Fs);
+	r14 = ch_td_pair(h(:,4),h(:,1),mf,Fs,b(4),b(1));
 	%15
-	r15 = ch_td_pair(h(:,5),h(:,1),mf,Fs);
+	r15 = ch_td_pair(h(:,5),h(:,1),mf,Fs,b(5),b(1));
 	%23
-	r23 = ch_td_pair(h(:,3),h(:,2),mf,Fs);
+	r23 = ch_td_pair(h(:,3),h(:,2),mf,Fs,b(3),b(2));
 	%24
-	r24 = ch_td_pair(h(:,4),h(:,2),mf,Fs);
+	r24 = ch_td_pair(h(:,4),h(:,2),mf,Fs,b(4),b(2));
 	%25
-	r25 = ch_td_pair(h(:,5),h(:,2),mf,Fs);
+	r25 = ch_td_pair(h(:,5),h(:,2),mf,Fs,b(5),b(2));
 	%34
-	r34 = ch_td_pair(h(:,4),h(:,3),mf,Fs);
+	r34 = ch_td_pair(h(:,4),h(:,3),mf,Fs,b(4),b(3));
 	%35
-	r35 = ch_td_pair(h(:,5),h(:,3),mf,Fs);
+	r35 = ch_td_pair(h(:,5),h(:,3),mf,Fs,b(5),b(3));
 	%45
-	r45 = ch_td_pair(h(:,5),h(:,4),mf,Fs);
+	r45 = ch_td_pair(h(:,5),h(:,4),mf,Fs,b(5),b(4));
 	
 	outArray = [r12,r13,r14,r15,r23,r24,r25,r34,r35,r45];
 
